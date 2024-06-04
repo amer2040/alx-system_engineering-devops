d71f142aab844bb918e99c911b1567f99ef0033c
amer2040 <aboodamer7@gmail.com>
2024-06-04 14:09:36 +0300

    api

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""function that prints the titles of the first top 10 hot posts listed"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """ print top 10 titles"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'reconnect/1.0'}
+
+    res = requests.get(url, headers=headers)
+    if res.status_code != 200:
+        print('None')
+    else:
+        top10 = res.json().get('data').get('children')
+        for x in range(10):
+            print(top10[x].get('data').get('title'))
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" function that returns a list containing
+the titles of all hot articles for a given subreddit."""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """ Returns a list containing the titles of all hot articles"""
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'reconnect/1.0'}
+    params = {"after": after}
+    res = requests.get(url, headers=headers, params=params)
+
+    if res.status_code != 200:
+        return None
+    else:
+        income = res.json().get('data').get('children')
+        for x in income:
+            data = x.get('data')
+            title = data.get('title')
+            hot_list.append('title')
+        after = res.json().get("data").get("after")
+        if after is None:
+            return hot_list
+        else:
+            return recurse(subreddit, hot_list, after)
+        
