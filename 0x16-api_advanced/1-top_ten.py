0988d5adddacce9678bad72d9f17c692361af394
amer2040 <aboodamer7@gmail.com>
2024-09-09 23:00:34 +0300

    update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
-"""
-this doc for module
-"""
+"""function that queries the reddit API and prints the
+titles of the first 10 hot posts listed for a given subreddit."""
+
+
 import requests
 
 
 def top_ten(subreddit):
-    """method doc"""
-    headers = {"User-Agent": "MyCustomUserAgent/1.0"}
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    response = requests.get(url, allow_redirects=False, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        for post in data["data"]["children"]:
-            print(post["data"]["title"])
-    else:
+    """function that queries the reddit API and prints the
+        titles of the first 10 hot posts listed for
+        a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    try:
+        response = requests.get(url, headers=headers,
+                                allow_redirects=False)
+        if response.status_code == 200:
+            children = response.json().get('data').get('children')
+            for i in range(10):
+                print(children[i].get('data').get('title'))
+        else:
+            print("None")
+    except Exception:
         print("None")
