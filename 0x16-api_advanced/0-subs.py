08c9161f8e3d87984df0974cf8bc8a613a8ab038
amer2040 <aboodamer7@gmail.com>
2024-09-09 22:37:06 +0300

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,18 @@
 #!/usr/bin/python3
 """
-This doc for module
+this doc for module
 """
-
 import requests
 
 headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
+
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers in a given subreddit."""
-    try:
-        url = f"https://www.reddit.com/r/{subreddit}/about.json"
-        response = requests.get(url, allow_redirects=False, headers=headers)
-        response.raise_for_status()  # Raise an exception for bad status codes
+    """method doc"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
         data = response.json()
         return data["data"]["subscribers"]
-    except (requests.RequestException, KeyError):
-        # Handle cases where the request fails or raises an exception
-        print(f"An error occurred: {response.status_code}")
+    else:
         return 0
