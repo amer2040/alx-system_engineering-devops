c9baa3937e9ec6396b2bcf43ced647588929837e
amer2040 <aboodamer7@gmail.com>
2024-09-09 22:56:07 +0300

    update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,11 +5,9 @@ this doc for module
 import requests
 
 
-headers = {"User-Agent": "MyCustomUserAgent/1.0"}
-
-
 def top_ten(subreddit):
     """method doc"""
+    headers = {"User-Agent": "MyCustomUserAgent/1.0"}
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     response = requests.get(url, allow_redirects=False, headers=headers)
     if response.status_code == 200:
